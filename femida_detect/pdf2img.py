82f7bf356e0b1e7d3410fb071d4873c40af235bc
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-08-18 13:12:43 +0300

    refactor script

--- femida_detect/pdf2img.py
+++ femida_detect/pdf2img.py
@@ -3,13 +3,16 @@ import PIL.Image
 import os
 from pathlib import Path
 
+command = ("gs  -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 "
+           "-dGridFitTT=2 -sDEVICE=pngalpha -dTextAlphaBits=4 -dGraphicsAlphaBits=4 "
+           "-r500x500 -sOutputFile={out} -f{path}")
+
 
 def pdf_to_images(path):
     with tempfile.TemporaryDirectory() as tmp:
         tmp = Path(tmp)
         out = Path(tmp, 'img%03d.png')
-        command = f"gs -dNOPAUSE -q -sDEVICE=png256 -r400 -dBATCH -o {out} {path}"
-        code = os.system(command)
+        code = os.system(command.format(out=out, path=path))
         if code != 0:
             raise ValueError('gs failed')
         return [PIL.Image.open(image).convert('RGB') for image in tmp.iterdir()]
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import os
 
 
-@pytest.fixture('session', params=['smallpdf.pdf', 'bigpdf.pdf'])
+@pytest.fixture('session', params=['smallpdf.pdf'])
 def pdf(request):
     return os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
