8640fcda5b19cb42824775c9dc826acd468b2af6
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-08-24 22:55:29 +0300

    refactor ssl script

--- frontend/app/prestart.sh
+++ frontend/app/prestart.sh
@@ -1,5 +1,3 @@
 mkdir -p "$UPLOAD_FOLDER"
 mkdir -p "$RESULTS_FOLDER"
 envsubst < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
-chown nginx:nginx /etc/nginx/ssl
-chmod 700 /etc/nginx/ssl
--- new_ssl.sh
+++ new_ssl.sh
@@ -1,2 +1,5 @@
 mkdir -p docker/ssl/
 openssl req -new -x509 -days 9999 -nodes -out docker/ssl/cert.pem -keyout docker/ssl/cert.key
+chown nginx:nginx /etc/nginx/ssl
+chmod 700 docker/ssl/
+
