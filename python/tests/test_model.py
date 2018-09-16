7ddae9af66d117624f4a958b0f31b1ae99dc3606
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-09-16 11:29:02 +0300

    fix small bugs

--- python/tests/test_model.py
+++ python/tests/test_model.py
@@ -10,7 +10,7 @@ from femida_detect.imgparse import RECTANGLES_POSITIONS_MIDDLE
 
 @pytest.mark.parametrize(
     ['size', 'v'],
-    itertools.product([20, 28, 30], ['v1', 'v2', 'v3'])
+    itertools.product([20, 28, 30], ['v1', 'v2', 'v3', 'v5'])
 )
 def test_model(size, v):
     inp = torch.rand(10, 3, size, size)
