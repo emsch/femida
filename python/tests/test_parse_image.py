8198fe412383948259ab9c6dac997a5ccb586340
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-12-22 18:04:58 +0300

    add tests

--- python/tests/test_parse_image.py
+++ python/tests/test_parse_image.py
@@ -3,7 +3,8 @@ import os
 import cv2
 from femida_detect.imgparse import (
     crop_image,
-    CroppedAnswers
+    CroppedAnswers,
+    need_flip
 )
 
 
@@ -15,3 +16,11 @@ def test_crop_image(jpg):
 def test_recognized_positions(cropped):
     recognized = CroppedAnswers.from_file(cropped).recognized_rectangles_image()
     pytest.helpers.save_image(recognized, 'test_recognized_positions', os.path.basename(cropped) + '.recognized.jpg')
+
+
+def test_flipping(jpg):
+    cropped = crop_image(cv2.imread(jpg))
+    assert not need_flip(cropped)
+    assert need_flip(cropped[::-1, ::-1])
+    cropped_answers = CroppedAnswers(cropped[::-1, ::-1])
+    assert not need_flip(cropped_answers.cropped)
